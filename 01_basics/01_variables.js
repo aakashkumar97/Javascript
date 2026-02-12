1e3e300b105bf038323d106e740cb3a49c82be14
Aakash Kumar <aakash.kumar733@gmail.com>
2026-02-12 08:57:34 +0530

    Prettify

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,20 +1,25 @@
-const accountId = 144553
-let accountEmail = "aakash@gmail.com"
-var accountPassword = "12345"
-accountCity = "Ghaziabad"
-let accountState
+const accountId = 144553;
+let accountEmail = "aakash@gmail.com";
+var accountPassword = "12345";
+accountCity = "Ghaziabad";
+let accountState;
 
 //accountId = 2
 
-accountEmail = "aakash@google.com"
-accountPassword = "1122330"
+accountEmail = "aakash@google.com";
+accountPassword = "1122330";
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
 */
 
-
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,3 @@
 "use strict"; //treat all JS code as newer version
 
 // alert("Hello") // alert will work onnly on browser, not on nodejs.
-
