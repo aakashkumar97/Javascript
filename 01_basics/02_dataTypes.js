3e8065c931330b1bf78101a7cd24effd12dffc9c
Aakash Kumar <aakash.kumar733@gmail.com>
2026-02-12 13:59:56 +0530

    update datatype

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,25 @@
-"use strict"; //treat all JS code as newer version
+"use strict"; // treat all JS code as newer version
 
-// alert("Hello") // alert will work onnly on browser, not on nodejs.
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("Aakash");
+
+let name = "Aakash";
+let age = 28;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
